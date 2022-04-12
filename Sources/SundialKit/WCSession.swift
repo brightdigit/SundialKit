4574789d6860b7ab160647ffd07b8336e5cc561a
Leo Dion <leogdion@brightdigit.com>
2022-04-11 20:39:47 -0400

    fixing for standard build

--- Sources/SundialKit/WCSession.swift
+++ Sources/SundialKit/WCSession.swift
@@ -1,19 +1,21 @@
-import WatchConnectivity
+#if canImport(WatchConnectivity)
+  import WatchConnectivity
 
-public extension WCSession {
-  var isPairedAppInstalled: Bool {
+  public extension WCSession {
+    var isPairedAppInstalled: Bool {
+      #if os(iOS)
+        return isWatchAppInstalled
+      #else
+        return isCompanionAppInstalled
+      #endif
+    }
+
     #if os(iOS)
-      return isWatchAppInstalled
-    #else
-      return isCompanionAppInstalled
+      static let isCompanionAppInstalledKeyPath: KeyPath<WCSession, Bool>
+        = \.isWatchAppInstalled
+    #elseif os(watchOS)
+      static let isCompanionAppInstalledKeyPath: KeyPath<WCSession, Bool>
+        = \.isCompanionAppInstalled
     #endif
   }
-
-  #if os(iOS)
-    static let isCompanionAppInstalledKeyPath: KeyPath<WCSession, Bool>
-      = \.isWatchAppInstalled
-  #elseif os(watchOS)
-    static let isCompanionAppInstalledKeyPath: KeyPath<WCSession, Bool>
-      = \.isCompanionAppInstalled
-  #endif
-}
+#endif
