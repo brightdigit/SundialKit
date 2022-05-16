c42399ed09bec9989d5da8db24cb7fb2d2e3e216
Leo Dion <leogdion@brightdigit.com>
2022-05-16 18:25:17 -0400

    Unhide dev dependencies

--- Package.swift
+++ Package.swift
@@ -12,10 +12,10 @@ let package = Package(
     )
   ],
   dependencies: [
-//    .package(url: "https://github.com/shibapm/Komondor", from: "1.1.2"), // dev
-//    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.0"), // dev
-//    .package(url: "https://github.com/realm/SwiftLint", from: "0.41.0"), // dev
-//    .package(url: "https://github.com/shibapm/Rocket", from: "1.2.0") // dev
+    .package(url: "https://github.com/shibapm/Komondor", from: "1.1.2"), // dev
+    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.0"), // dev
+    .package(url: "https://github.com/realm/SwiftLint", from: "0.41.0"), // dev
+    .package(url: "https://github.com/shibapm/Rocket", from: "1.2.0") // dev
   ],
   targets: [
     .target(
