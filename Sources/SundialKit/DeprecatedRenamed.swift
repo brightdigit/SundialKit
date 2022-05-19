fe6d1dc4e7f7a2bb69d56e780bd366c877a1eb48
Leo Dion <leogdion@brightdigit.com>
2022-05-19 11:43:30 -0400

    adding old names for deprecated typealiases

---
+++ Sources/SundialKit/DeprecatedRenamed.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+@available(iOS 14.2, watchOS 7.1, macOS 11.0, *)
+@available(*, deprecated, renamed: "NetworkObject")
+public typealias NWObject = NetworkObject
+
+@available(*, deprecated, renamed: "PathStatus")
+public typealias NWPathStatus = PathStatus
