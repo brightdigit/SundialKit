edf2e25e04ec480c98e482c76d05025ad9e1997c
Leo Dion <leogdion@brightdigit.com>
2025-10-16 16:22:42 -0400

    Fixing Workflows

--- Tests/SundialKitTests/Network/Extensions/PathStatusNetworkTests.swift
+++ Tests/SundialKitTests/Network/Extensions/PathStatusNetworkTests.swift
@@ -34,44 +34,44 @@ struct PathStatusNetworkTests {
   #endif
 
   #if canImport(Network)
-  @Test(
-    "PathStatus initialized from NWPath properties",
-    arguments: [
-      (
-        NWPath.Status.satisfied, NWPath.UnsatisfiedReason.cellularDenied,
-        [PathStatus.Interface.loopback, .cellular], PathStatus.satisfied([.loopback, .cellular])
-      ),
-      (
-        NWPath.Status.requiresConnection, NWPath.UnsatisfiedReason.cellularDenied,
-        [PathStatus.Interface.loopback, .cellular], PathStatus.requiresConnection
-      ),
-      (
-        NWPath.Status.unsatisfied, NWPath.UnsatisfiedReason.cellularDenied,
-        [PathStatus.Interface.loopback, .cellular], PathStatus.unsatisfied(.cellularDenied)
-      ),
-      (
-        NWPath.Status.unsatisfied, NWPath.UnsatisfiedReason.notAvailable,
-        [PathStatus.Interface.loopback, .cellular], PathStatus.unsatisfied(.notAvailable)
-      ),
-      (
-        NWPath.Status.unsatisfied, NWPath.UnsatisfiedReason.localNetworkDenied,
-        [PathStatus.Interface.loopback, .cellular], PathStatus.unsatisfied(.localNetworkDenied)
-      ),
-      (
-        NWPath.Status.unsatisfied, NWPath.UnsatisfiedReason.wifiDenied,
-        [PathStatus.Interface.loopback, .cellular], PathStatus.unsatisfied(.wifiDenied)
-      ),
-    ])
-  @available(macOS 11.0, iOS 14.2, watchOS 7.1, tvOS 14.2, *)
-  func pathStatusInit(
-    status: NWPath.Status,
-    reason: NWPath.UnsatisfiedReason,
-    interfaces: [PathStatus.Interface],
-    expected: PathStatus
-  ) throws {
-    #if canImport(Network)
-      assertStatus(status, reason: reason, interfaces: interfaces, equalsPathStatus: expected)
-    #endif
-  }
+    @Test(
+      "PathStatus initialized from NWPath properties",
+      arguments: [
+        (
+          NWPath.Status.satisfied, NWPath.UnsatisfiedReason.cellularDenied,
+          [PathStatus.Interface.loopback, .cellular], PathStatus.satisfied([.loopback, .cellular])
+        ),
+        (
+          NWPath.Status.requiresConnection, NWPath.UnsatisfiedReason.cellularDenied,
+          [PathStatus.Interface.loopback, .cellular], PathStatus.requiresConnection
+        ),
+        (
+          NWPath.Status.unsatisfied, NWPath.UnsatisfiedReason.cellularDenied,
+          [PathStatus.Interface.loopback, .cellular], PathStatus.unsatisfied(.cellularDenied)
+        ),
+        (
+          NWPath.Status.unsatisfied, NWPath.UnsatisfiedReason.notAvailable,
+          [PathStatus.Interface.loopback, .cellular], PathStatus.unsatisfied(.notAvailable)
+        ),
+        (
+          NWPath.Status.unsatisfied, NWPath.UnsatisfiedReason.localNetworkDenied,
+          [PathStatus.Interface.loopback, .cellular], PathStatus.unsatisfied(.localNetworkDenied)
+        ),
+        (
+          NWPath.Status.unsatisfied, NWPath.UnsatisfiedReason.wifiDenied,
+          [PathStatus.Interface.loopback, .cellular], PathStatus.unsatisfied(.wifiDenied)
+        ),
+      ])
+    @available(macOS 11.0, iOS 14.2, watchOS 7.1, tvOS 14.2, *)
+    func pathStatusInit(
+      status: NWPath.Status,
+      reason: NWPath.UnsatisfiedReason,
+      interfaces: [PathStatus.Interface],
+      expected: PathStatus
+    ) throws {
+      #if canImport(Network)
+        assertStatus(status, reason: reason, interfaces: interfaces, equalsPathStatus: expected)
+      #endif
+    }
   #endif
 }
