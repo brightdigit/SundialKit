c6b64c6f95b9075c1a8a43e3774043cad38a114f
Leo Dion <leogdion@brightdigit.com>
2025-11-19 16:17:48 -0500

    removing enum

--- Sources/SundialKit/SundialKit.swift
+++ Sources/SundialKit/SundialKit.swift
@@ -31,16 +31,3 @@
 // Re-export all modules for convenience
 @_exported import SundialKitCore
 @_exported import SundialKitNetwork
-
-/// SundialKit is an umbrella module that re-exports all core functionality.
-///
-/// Import this module to get access to all SundialKit features:
-/// - SundialKitCore: Protocol definitions and base types
-/// - SundialKitNetwork: Network monitoring implementation
-/// - SundialKitConnectivity: WatchConnectivity management
-///
-/// For more fine-grained control, you can import individual modules directly.
-public enum SundialKit {
-  /// The current version of SundialKit
-  public static let version = "2.0.0"
-}
